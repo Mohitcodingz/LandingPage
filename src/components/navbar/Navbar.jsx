d0721148289960dd15d0cb8db1ef804eb78b0c8f
Mohitcodingz <Mohitsingh2003uk@gmail.com>
2026-06-12 16:46:52 +0530

    Fix logo import case sensitivity

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/Logo.png'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
